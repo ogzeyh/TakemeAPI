de601017f3b88c6d25a7ef8cd18f88a9fb520e84
ogzeyh <87442381+ogzeyh@users.noreply.github.com>
2025-11-05 19:16:37 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,5 +17,4 @@ app.options('*', cors());
 
 app.use("/api/url", urlRoutes);
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+export default app;
