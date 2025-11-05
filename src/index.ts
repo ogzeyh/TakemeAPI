da4274fbc65897a7868f8c9ec911f3532a327214
ogzeyh <87442381+ogzeyh@users.noreply.github.com>
2025-11-05 18:56:38 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,17 @@ const port = process.env.PORT || '4000';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      process.env.PRODUCTION_URL || "https://takeme-kappa.vercel.app",
+      process.env.LOCAL_URL || "http://localhost:3000",
+    ],
+    methods: "*",
+    allowedHeaders: "*",
+    credentials: true
+  })
+);
 
 app.use('/api/url', urlRoutes)
 
